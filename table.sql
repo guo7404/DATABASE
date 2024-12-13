15e7f7d96398cbf1cd5cae5af37767aedf368847
김현준 <102206431+guo7404@users.noreply.github.com>
2024-12-13 14:40:48 +0900

    시퀀스 업데이트 for table.sql

--- table.sql
+++ table.sql
@@ -47,3 +47,16 @@ CREATE TABLE Typhoon_Rating_System (
     rname VARCHAR2(50) CONSTRAINT TRS_rname_NN NOT NULL,
     CONSTRAINT TRS_std_range_CHK CHECK (minstd <= maxstd)
 );
+
+create sequence impact_impno_seq
+                start with 1
+                INCREMENT by 1;
+create sequence region_regno_seq
+                start with 1
+                INCREMENT by 1; 
+create sequence weather_wno_seq
+                start with 1
+                INCREMENT by 1;
+create sequence trs_rno_seq
+                start with 10
+                INCREMENT by 10;
